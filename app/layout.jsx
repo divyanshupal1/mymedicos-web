ebf3b690ccc3dc8812769731a84039b6b9fad053
divyanshuviragvigyan <divyanshu.pal.viragvigyan@gmail.com>
2024-11-22 18:06:47 +0530

    ganalytics next

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,8 @@ import { Toaster } from "@/components/ui/toaster"
 import { Navbar } from "@/components/navigation/navbar";
 import { cn } from "@/lib/utils";
 import Doubt from "@/components/doubt/doubt";
+import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +17,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-YMJPH29S40"></script>
+      </head>
       <body className={cn(inter.className,"overflow-x-hidden min-h-screen")}>
+        <GoogleTagManager gtmId="GTM-T8MN5ZTC" />
         <div className="w-full h-screen flex flex-col items-start justify-start">
             <Navbar/>
             <div id="main-data" className="w-full h-full overflow-y-scroll">
@@ -23,6 +29,7 @@ export default function RootLayout({ children }) {
             </div>
         </div>
         <Toaster />
+        <GoogleAnalytics gaId="G-YMJPH29S40"/>
       </body>
     </html>
   );
